6f159e75050c43c48718c96a29211d056daef769
rasulalizadeh <mohammad.alizadeh12@gmail.com>
2021-11-05 18:19:51 +0330

    fixed media relation

--- src/Traits/HasMedia.php
+++ src/Traits/HasMedia.php
@@ -4,6 +4,7 @@ namespace Ruinton\Traits;
 
 use Illuminate\Support\Str;
 use Ruinton\Models\Media;
+use Spatie\Multitenancy\Models\Tenant;
 
 trait HasMedia
 {
@@ -13,15 +14,15 @@ trait HasMedia
     }
 
     public function mediaTableName() {
-        return Str::singular($this->getTable()).'_media';
+        return Tenant::current()->getDatabaseName().'.'.Str::singular($this->getTable()).'_media';
     }
 
     public function belongsToMedia($mediaType = null) {
         if($mediaType) {
-            return $this->connection('tenant')->belongsToMany(Media::class, $this->mediaTableName(), null, 'media_id')
+            return $this->belongsToMany(Media::class, $this->mediaTableName(), null, 'media_id')
                 ->where('media.media_type_id', '=', $mediaType);
         }else {
-            return $this->connection('tenant')->belongsToMany(Media::class, $this->mediaTableName(), null, 'media_id');
+            return $this->belongsToMany(Media::class, $this->mediaTableName(), null, 'media_id');
         }
     }
 }
